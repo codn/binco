337a1465d77f6c7b45acedbc853fc0a799f8e933
vicocamacho <vicocamacho88@gmail.com>
2015-03-18 19:05:48 -0600

    Add grouping stylesheet and bootstrap form tag

--- app/helpers/bootstrap_on_rails/bootstrap_form_helper.rb
+++ app/helpers/bootstrap_on_rails/bootstrap_form_helper.rb
@@ -4,5 +4,10 @@ module BootstrapOnRails
       options[:builder] = BootstrapFormBuilder
       form_for(record, options, &block)
     end
+
+    def bootstrap_form_tag(url_for_options = {}, options = {}, &block)
+      options[:acts_like_form_tag] = true
+      bootstrap_form_for("", options, &block)
+    end
   end
 end
