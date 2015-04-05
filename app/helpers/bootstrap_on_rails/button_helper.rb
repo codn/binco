b7c7f504559f6823e395d04a98d827726eaff4d1
vicocamacho <vicocamacho88@gmail.com>
2015-04-05 13:35:17 -0500

    Add Button Helpers

--- app/helpers/bootstrap_on_rails/button_helper.rb
+++ app/helpers/bootstrap_on_rails/button_helper.rb
@@ -1,21 +1,41 @@
 module BootstrapOnRails
   module ButtonHelper
-    def button_primary(title, options = {})
+    def button(title, options = {})
+      options[:type] ||= :default
       options[:class] ||= ''
-      options[:class] << 'btn-primary'
-      default_button(title, options)
+      options[:class] << ' ' if options[:class].length > 1
+      options[:class] << "btn btn-#{options[:type]}"
+      content_tag :button, title, options.except(:type)
+    end
+
+    def button_primary(title, options = {})
+      options[:type] = :primary
+      button(title, options)
     end
 
     def button_default(title, options = {})
-      options[:class] ||= ''
-      options[:class] << 'btn-default'
-      default_button(title, options)
+      options[:type] = :default
+      button(title, options)
     end
 
-    private
-      def default_button(title, options = {})
-        options[:class] << ' btn'
-        content_tag :button, title, options
-      end
+    def button_success(title, options = {})
+      options[:type] = :default
+      button(title, options)
+    end
+
+    def button_info(title, options = {})
+      options[:type] = :info
+      button(title, options)
+    end
+
+    def button_warning(title, options = {})
+      options[:type] = :warning
+      button(title, options)
+    end
+
+    def button_danger(title, options = {})
+      options[:type] = :warning
+      button(title, options)
+    end
   end
 end
---
+++ app/helpers/bootstrap_on_rails/button_link_helper.rb
@@ -0,0 +1,41 @@
+module BootstrapOnRails
+  module ButtonLinkHelper
+    def button_link_to(name = nil, options = nil, html_options = {}, &block)
+      html_options[:type] ||= :default
+      html_options[:class] ||= ''
+      html_options[:class] << ' ' if html_options[:class].length > 1
+      html_options[:class] << "btn btn-#{html_options[:type]}"
+      link_to(name, options, html_options.except(:type), &block)
+    end
+
+    def button_primary_link_to(name = nil, options = nil, html_options = {}, &block)
+      html_options[:type] = :primary
+      button_link_to(name, options, html_options, &block)
+    end
+
+    def button_default_link_to(name = nil, options = nil, html_options = {}, &block)
+      html_options[:type] = :default
+      button_link_to(name, options, html_options, &block)
+    end
+
+    def button_danger_link_to(name = nil, options = nil, html_options = {}, &block)
+      html_options[:type] = :danger
+      button_link_to(name, options, html_options, &block)
+    end
+
+    def button_warning_link_to(name = nil, options = nil, html_options = {}, &block)
+      html_options[:type] = :warning
+      button_link_to(name, options, html_options, &block)
+    end
+
+    def button_success_link_to(name = nil, options = nil, html_options = {}, &block)
+      html_options[:type] = :success
+      button_link_to(name, options, html_options, &block)
+    end
+
+    def button_info_link_to(name = nil, options = nil, html_options = {}, &block)
+      html_options[:type] = :info
+      button_link_to(name, options, html_options, &block)
+    end
+  end
+end
