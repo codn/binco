da6cbd6d3e6e562437259e7f33df974d3fe64771
Victor Camacho <victor@codn.mx>
2019-08-19 17:16:42 -0500

    Conditional load of will paginate

--- app/helpers/binco/pagination_renderer.rb
+++ app/helpers/binco/pagination_renderer.rb
@@ -1,65 +1,66 @@
-require 'will_paginate/view_helpers/action_view'
+if defined?(::WillPaginate)
+  require 'will_paginate/view_helpers/action_view'
 
-module Binco
-  class PaginationRenderer < WillPaginate::ActionView::LinkRenderer
-    ELLIPSIS = '&hellip;'
+  module Binco
+    class PaginationRenderer < WillPaginate::ActionView::LinkRenderer
+      ELLIPSIS = '&hellip;'
 
-    def to_html
-      caca
-      list_items = pagination.map do |item|
-        case item
-          when Fixnum
-            page_number(item)
-          else
-            send(item)
+      def to_html
+        list_items = pagination.map do |item|
+          case item
+            when Fixnum
+              page_number(item)
+            else
+              send(item)
+          end
+        end.join(@options[:link_separator])
+
+        list_wrapper = tag :ul, list_items, class: 'pagination', role: 'group'
+        tag :nav, list_wrapper, class: @options[:class]
+      end
+
+      def container_attributes
+        super.except(*[:link_options])
+      end
+
+      protected
+
+      def page_number(page)
+        link_options = @options[:link_options] || {}
+
+        if page == current_page
+          tag :li, tag(:span, page, class: 'page-link'), class: 'page-item active'
+        else
+          link_options.merge! class: 'page-link', rel: rel_value(page)
+          tag :li, link(page, page, link_options), class: 'page-item'
         end
-      end.join(@options[:link_separator])
-
-      list_wrapper = tag :ul, list_items, class: 'pagination', role: 'group'
-      tag :nav, list_wrapper, class: @options[:class]
-    end
-
-    def container_attributes
-      super.except(*[:link_options])
-    end
-
-    protected
-
-    def page_number(page)
-      link_options = @options[:link_options] || {}
-
-      if page == current_page
-        tag :li, tag(:span, page, class: 'page-link'), class: 'page-item active'
-      else
-        link_options.merge! class: 'page-link', rel: rel_value(page)
-        tag :li, link(page, page, link_options), class: 'page-item'
       end
-    end
 
-    def previous_or_next_page(page, text, classname)
-      link_options = @options[:link_options] || {}
+      def previous_or_next_page(page, text, classname)
+        link_options = @options[:link_options] || {}
 
-      if page
-        link_wrapper = link(text, page, link_options.merge(class: 'page-link'))
-        tag :li, link_wrapper, class: '%s page-item' % classname
-      else
-        span_wrapper = tag(:span, text, class: 'page-link')
-        tag :li, span_wrapper, class: '%s page-item disabled' % classname
+        if page
+          link_wrapper = link(text, page, link_options.merge(class: 'page-link'))
+          tag :li, link_wrapper, class: '%s page-item' % classname
+        else
+          span_wrapper = tag(:span, text, class: 'page-link')
+          tag :li, span_wrapper, class: '%s page-item disabled' % classname
+        end
       end
-    end
 
-    def gap
-      tag :li, tag(:i, ELLIPSIS, class: 'page-link'), class: 'page-item disabled'
-    end
+      def gap
+        tag :li, tag(:i, ELLIPSIS, class: 'page-link'), class: 'page-item disabled'
+      end
 
-    def previous_page
-      num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page num, @options[:previous_label], 'previous'
-    end
+      def previous_page
+        num = @collection.current_page > 1 && @collection.current_page - 1
+        previous_or_next_page num, @options[:previous_label], 'previous'
+      end
 
-    def next_page
-      num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
-      previous_or_next_page num, @options[:next_label], 'next'
+      def next_page
+        num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
+        previous_or_next_page num, @options[:next_label], 'next'
+      end
     end
   end
 end
